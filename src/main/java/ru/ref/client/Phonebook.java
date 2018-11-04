a1c2d66f874196eb46491e468fbc59f7656b0760
Nikita <nikita9609@mail.ru>
2018-11-05 02:11:47 +0300

    d

--- src/main/java/ru/ref/client/Phonebook.java
+++ src/main/java/ru/ref/client/Phonebook.java
@@ -2,41 +2,90 @@ package ru.ref.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Grid;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.*;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.*;
 
 public class Phonebook implements EntryPoint {
 
+    private ServiceAsync service = GWT.create(Service.class);
+
+    private FlexTable table = new FlexTable();
+    private TextBox id;
+    private TextBox name;
+    private TextBox address;
+    private TextBox phone;
     private VerticalPanel mainPanel = new VerticalPanel();
 
-    Grid grid = new Grid(ContactList.getAllContacts().size(),4);
+    AsyncCallback<ContactTable> contactTableCallback = new AsyncCallback<ContactTable>() {
+        @Override
+        public void onFailure(Throwable throwable) {
+            String details = throwable.getMessage();
+
+            System.out.println(details);
+        }
+
+        @Override
+        public void onSuccess(ContactTable result) {
+            table.removeAllRows();
+
+            table.setBorderWidth(6);
+
+            table.setText(0, 0, "id");
+            table.setText(0, 1, "name");
+            table.setText(0, 2, "address");
+            table.setText(0, 3, "phone");
+            table.setCellPadding(6);
+
+            mainPanel.add(table);
+
+            for (int i = 0; i < result.getIdArray().length; i++) {
+                //Button updateBtn = new Button("update");
+
+                id = new TextBox();
+                id.setText(String.valueOf(result.getIdArray()[i]));
+                id.setReadOnly(true);
+
+                name = new TextBox();
+                name.setText(result.getNameArray()[i]);
+
+                address = new TextBox();
+                address.setText(result.getAddressArray()[i]);
+
+                phone = new TextBox();
+                phone.setText(result.getPhoneArray()[i]);
+
+                final int row = table.getRowCount();
+
+                table.setWidget(row, 0, id);
+                table.setWidget(row, 1, name);
+                table.setWidget(row, 2, address);
+                table.setWidget(row, 3, phone);
+
+            }
+        }
+    };
 
     public void onModuleLoad() {
+        mainPanel.add(table);
+        mainPanel.setWidth("100%");
+        mainPanel.setHeight("100%");
+        mainPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+        mainPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
 
-        grid.setBorderWidth(3);
+        RootPanel.get("mainContainer").add(mainPanel);
 
-        grid.setText(0, 0, Integer.toString(ContactList.getAllContacts().get(0).getId()));
-        grid.setText(0, 1, ContactList.getAllContacts().get(0).getName());
-        grid.setText(0, 2, ContactList.getAllContacts().get(0).getAddress());
-        grid.setText(0, 3, ContactList.getAllContacts().get(0).getPhone());
-        grid.setText(1, 0, " ");
-        grid.setText(1, 1, " ");
-        grid.setText(1, 2, " ");
-        grid.setText(1, 3, " ");
-        grid.setText(2, 0, " ");
-        grid.setText(2, 1, " ");
-        grid.setText(2, 2, " ");
-        grid.setText(2, 3, " ");
-        grid.setText(3, 0, " ");
-        grid.setText(3, 1, " ");
-        grid.setText(3, 2, " ");
-        grid.setText(3, 3, " ");
-        grid.setText(4, 0, " ");
-        grid.setText(4, 1, " ");
-        grid.setText(4, 2, " ");
-        grid.setText(4, 3, " ");
-       mainPanel.add(grid);
-       RootPanel.get("mainContainer").add(grid);
+        ContactTable();
+
+
+    }
+    public void ContactTable() {
+        if (service == null) {
+            service = GWT.create(Service.class);
+        }
+
+        service.getContacts(contactTableCallback);
     }
 }
